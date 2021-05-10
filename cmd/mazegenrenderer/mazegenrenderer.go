ff4b8ddc584bdc949ecb15af1d1c76574fc46626
Michael Moore <mike@miketmoore.com>
2021-05-10 14:07:57 -0400

    wip

--- cmd/mazegenrenderer/mazegenrenderer.go
+++ cmd/mazegenrenderer/mazegenrenderer.go
@@ -14,14 +14,32 @@ import (
 	"golang.org/x/image/font/basicfont"
 )
 
+type MazeDrawData struct {
+	rows, columns                                    int
+	originX, originY, cellSize, wallWidth, thickness float64
+	drawWalls                                        bool
+}
+
 func run() {
 
-	debug := true
-	drawWalls := false
+	argsWithoutProg := os.Args[1:]
+
+	debug := false
+	drawWalls := true
+	if len(argsWithoutProg) > 0 {
+		for _, value := range argsWithoutProg {
+			if value == "debug" {
+				debug = true
+			}
+		}
+	}
+
+	fmt.Printf("debug=%t\n", debug)
+	fmt.Printf("drawWalls=%t\n", drawWalls)
 
 	// Initialize window
 	fmt.Println("initializing window...")
-	win, err := pixelgl.NewWindow(
+	window, err := pixelgl.NewWindow(
 		pixelgl.WindowConfig{
 			Title:  "Maze",
 			Bounds: pixel.R(0, 0, 800, 800),
@@ -39,29 +57,38 @@ func run() {
 	// Initialize text
 	// orig := pixel.V(20, 50)
 	atlas := text.NewAtlas(basicfont.Face7x13, text.ASCII)
-	txt := text.New(pixel.V(20, 50), atlas)
-	// txt := text.New(orig, text.Atlas7x13)
-	txt.Color = colornames.Green
+	text := text.New(pixel.V(20, 50), atlas)
+	// text := text.New(orig, text.Atlas7x13)
+	text.Color = colornames.Green
 	fmt.Println("text initialized")
 
 	state := "buildmaze"
 
 	var grid *mazegen.Grid
 
-	for !win.Closed() {
+	mazeDrawData := &MazeDrawData{
+		rows:      15,
+		columns:   15,
+		originX:   10,
+		originY:   740,
+		cellSize:  50,
+		wallWidth: 2,
+		thickness: 0,
+		drawWalls: true,
+	}
+
+	for !window.Closed() {
 
 		// Quit application when user input matches
-		if win.JustPressed(pixelgl.KeyQ) {
+		if window.JustPressed(pixelgl.KeyQ) {
 			os.Exit(1)
 		}
 
 		switch state {
 		case "buildmaze":
-			rows := 15
-			cols := 15
 			random := mazegen.NewRandom()
 			grid = nil
-			grid, err = mazegen.BuildMaze(rows, cols, random)
+			grid, err = mazegen.BuildMaze(mazeDrawData.rows, mazeDrawData.columns, random)
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(0)
@@ -69,69 +96,88 @@ func run() {
 
 			state = "render"
 		case "render":
-			win.Clear(colornames.Black)
-			originX := 10.0
-			originY := 740.0
-			cellSize := 50.0
-			wallWidth := 2.0
-			thickness := 0.0
+			window.Clear(colornames.Black)
 
-			count := 0
-			for y, cells := range grid.Cells {
-				drawY := originY - (float64(y) * cellSize)
-				for x, cell := range cells {
+			// originX := 10.0
+			// originY := 740.0
+			// cellSize := 50.0
+			// wallWidth := 2.0
+			// thickness := 0.0
 
-					drawX := originX + (float64(x) * cellSize)
-					rectShape := buildRectangle(drawX, drawY, cellSize, cellSize, colornames.White, 1)
-					rectShape.Draw(win)
-
-					if drawWalls {
-						if cell.Walls[mazegen.North] {
-							buildRectangle(drawX, drawY+(cellSize-wallWidth), wallWidth, cellSize, colornames.Blue, thickness).Draw(win)
-						}
-						if cell.Walls[mazegen.East] {
-							buildRectangle(drawX+(cellSize-wallWidth), drawY, cellSize, wallWidth, colornames.Blue, thickness).Draw(win)
-						}
-						if cell.Walls[mazegen.South] {
-							buildRectangle(drawX, drawY, wallWidth, cellSize, colornames.Blue, thickness).Draw(win)
-						}
-						if cell.Walls[mazegen.West] {
-							buildRectangle(drawX, drawY, cellSize, wallWidth, colornames.Blue, thickness).Draw(win)
-						}
-					}
-
-					if debug {
-						// message := fmt.Sprintf("%d,%d", y, x)
-						message := fmt.Sprintf("%d", count)
-						txt.Clear()
-						fmt.Fprintln(txt, message)
-					}
-
-					// txt.Color = colornames.Green
-					// fmt.Fprintln(txt, message)
-					rect := pixel.R(drawX, drawY, drawX+cellSize, drawY+cellSize)
-					// txt.Draw(win, pixel.IM.Moved(rect.Center().Sub(txt.Bounds().Center())))
-
-					cellCenter := rect.Center()
-					vectorDiff := cellCenter.Sub(txt.Bounds().Center())
-
-					// matrix := pixel.IM.Moved(rect.Center().Sub(txt.Bounds().Center()))
-					// matrix := pixel.IM.Scaled(txt.Orig, 2)
-					matrix := pixel.IM.Moved(vectorDiff)
-					matrix = matrix.Scaled(txt.Orig, 1)
-					txt.Draw(win, matrix)
-					count++
-				}
-			}
+			drawMaze(debug, text, window, grid, mazeDrawData)
 			state = "view"
 		case "view":
-			if win.JustPressed(pixelgl.KeyEnter) {
+			if window.JustPressed(pixelgl.KeyEnter) {
 				state = "buildmaze"
 			}
 		}
 
-		win.Update()
+		window.Update()
 
+	}
+}
+
+func drawMaze(
+	debug bool,
+	text *text.Text,
+	window *pixelgl.Window,
+	grid *mazegen.Grid,
+	mazeDrawData *MazeDrawData,
+) {
+
+	originX := mazeDrawData.originX
+	originY := mazeDrawData.originY
+	cellSize := mazeDrawData.cellSize
+	wallWidth := mazeDrawData.wallWidth
+	thickness := mazeDrawData.thickness
+	drawWalls := mazeDrawData.drawWalls
+
+	count := 0
+	for y, cells := range grid.Cells {
+		drawY := originY - (float64(y) * cellSize)
+		for x, cell := range cells {
+
+			drawX := originX + (float64(x) * cellSize)
+			rectShape := buildRectangle(drawX, drawY, cellSize, cellSize, colornames.White, 1)
+			rectShape.Draw(window)
+
+			if drawWalls {
+				if cell.Walls[mazegen.North] {
+					buildRectangle(drawX, drawY+(cellSize-wallWidth), wallWidth, cellSize, colornames.Blue, thickness).Draw(window)
+				}
+				if cell.Walls[mazegen.East] {
+					buildRectangle(drawX+(cellSize-wallWidth), drawY, cellSize, wallWidth, colornames.Blue, thickness).Draw(window)
+				}
+				if cell.Walls[mazegen.South] {
+					buildRectangle(drawX, drawY, wallWidth, cellSize, colornames.Blue, thickness).Draw(window)
+				}
+				if cell.Walls[mazegen.West] {
+					buildRectangle(drawX, drawY, cellSize, wallWidth, colornames.Blue, thickness).Draw(window)
+				}
+			}
+
+			if debug {
+				// message := fmt.Sprintf("%d,%d", y, x)
+				message := fmt.Sprintf("%d", count)
+				text.Clear()
+				fmt.Fprintln(text, message)
+			}
+
+			// text.Color = colornames.Green
+			// fmt.Fprintln(text, message)
+			rect := pixel.R(drawX, drawY, drawX+cellSize, drawY+cellSize)
+			// text.Draw(window, pixel.IM.Moved(rect.Center().Sub(text.Bounds().Center())))
+
+			cellCenter := rect.Center()
+			vectorDiff := cellCenter.Sub(text.Bounds().Center())
+
+			// matrix := pixel.IM.Moved(rect.Center().Sub(text.Bounds().Center()))
+			// matrix := pixel.IM.Scaled(text.Orig, 2)
+			matrix := pixel.IM.Moved(vectorDiff)
+			matrix = matrix.Scaled(text.Orig, 1)
+			text.Draw(window, matrix)
+			count++
+		}
 	}
 }
 
